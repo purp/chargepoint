1e8aa7691f72380a4391963932145ec6cd1bf43d
Jim Meyer <jim@geekdaily.org>
2014-12-27 01:42:54 -0800

    Added .get_charge_spots

--- lib/chargepoint/api.rb
+++ lib/chargepoint/api.rb
@@ -1,4 +1,6 @@
 require 'mechanize'
+require 'uri'
+require 'geocoder'
 
 class ChargePoint
   class API
@@ -6,11 +8,14 @@ class ChargePoint
     @@agent = Mechanize.new { |agent|
       agent.user_agent_alias = 'Mac Safari'
     }
-    
+
+    # TODO: automatically wrap these methods with authentication guard condition
+    AUTHENTICATED_METHODS = [:get_charge_spots]
+
     def self.agent
       @@agent
     end
-    
+
     def self.authenticated?
       !!@@authenticated
     end
@@ -19,8 +24,71 @@ class ChargePoint
     # TODO: add method to see if authentication is still valid
     def self.authenticate(params)
       response = JSON[agent.post('https://na.chargepoint.com/users/validate', params).content]
-            
+
       @@authenticated = response['auth']
+    end
+
+    def self.get_charge_spots(latitude, longitude, search_radius=0.25, options={})
+      raise 'method requires authentication' unless authenticated?
+
+      params = params_with_filters(options)
+      params.merge!(search_box_for(latitude, longitude, search_radius))
+
+      uri = dashboard_uri_for_method
+      uri.query = URI.encode_www_form(params)
+
+      JSON[agent.get(uri.to_s).content]
+    end
+
+    private
+    def self.dashboard_uri_for_method
+      endpoint = caller(1)[0].scan(/\`([^']*)\'$/).flatten.first.gsub(/_([a-z])/) { $1.upcase }
+      URI.join('https://na.chargepoint.com/dashboard/', endpoint)
+    end
+
+    def self.search_box_for(latitude, longitude, search_radius)
+      search_box = {}
+
+      search_box[:lat], search_box[:lng] = latitude, longitude
+      search_box[:sw_lat], search_box[:sw_lng], search_box[:ne_lat], search_box[:ne_lng] = Geocoder::Calculations.bounding_box([latitude, longitude], search_radius)
+
+      search_box
+    end
+
+    def self.params_with_filters(options)
+      default_filters = {
+        :estimationfee => false,
+        :available => true,
+        :inuse => true,
+        :unknown => true,
+        :cp => true,
+        :other => true,
+        :l3 => true,
+        :l2 => true,
+        :l1 => false,
+        :estimate => false,
+        :fee => true,
+        :free => true,
+        :reservable => false,
+        :shared => true,
+        :chademo => true,
+        :saecombo => true,
+        :tesla => true,
+      }
+      default_options = {
+        :sort_by => 'distance',
+        :driver_connected_station_only => false,
+        :community_enabled_only => false
+      }
+
+      filters = default_filters.merge(options.delete(:filters) || {})
+
+      params = default_options.merge(options)
+      params.merge!(Hash[filters.map {|k,v| [('f_%s' % k.to_s).to_sym, v]}])
+
+      params[:_] = Time.now.to_i
+
+      params
     end
   end
 end
--- spec/chargepoint/api_spec.rb
+++ spec/chargepoint/api_spec.rb
@@ -12,6 +12,11 @@ def expect_auth(auth_status=true)
   expect(ChargePoint::API.agent).to receive(:post).with('https://na.chargepoint.com/users/validate', anything).and_return(response)
 end
 
+def expect_get
+  response = double_response('{}')
+  expect(ChargePoint::API.agent).to receive(:get).with(include('dashboard', 'getChargeSpots', 'f_reservable', 'community_enabled_only')).and_return(response)
+end
+
 def double_agent
   @original_agent = ChargePoint::API.agent
   ChargePoint::API.class_variable_set(:@@agent, double("Agent"))
@@ -26,8 +31,73 @@ RSpec.describe ChargePoint::API do
 
   it {is_expected.to respond_to(:authenticate)}
 
+  it 'should have a list of methods requiring authentication' do
+    expect(subject::AUTHENTICATED_METHODS).to be_kind_of(Array)
+  end
+
   it 'should have a Mechanize agent' do
     expect(subject.agent.class).to be(Mechanize)
+  end
+
+  context '.dashboard_uri_for_method' do
+    def testing_dashboard_uri
+      ChargePoint::API.send(:dashboard_uri_for_method)
+    end
+
+    subject { @uri = testing_dashboard_uri }
+
+    it { is_expected.to be_a(URI) }
+
+    it 'should start with the dashboard URI' do
+      expect(subject.to_s).to start_with('https://na.chargepoint.com/dashboard/')
+    end
+
+    it 'should end with the calling method name camel-cased' do
+      expect(subject.to_s).to end_with('testingDashboardUri')
+    end
+  end
+
+  context '.search_box_for' do
+    it 'should have keys for center, southwest, and northeast corners' do
+      search_box = subject.send(:search_box_for, 0, 0, 1)
+      expect(search_box.keys).to contain_exactly(:lat, :lng, :sw_lat, :sw_lng, :ne_lat, :ne_lng)
+    end
+  end
+  
+  context '.params_with_filters' do
+    before :each do |example|
+      options = example.metadata[:options] || {}
+      @params = ChargePoint::API.send(:params_with_filters, options)
+    end
+
+    it 'should prefix filters with f_', :options => {:filters => {:foo => true}} do
+      expect(@params).to include(:f_foo)
+      expect(@params).not_to include(:foo)
+    end
+    
+    it 'should not prefix options', :options => {:foo => true} do
+      expect(@params).to include(:foo)
+      expect(@params).not_to include(:f_foo)
+    end
+
+    it 'should remove filters from options', :options => {:filters => {:foo => true}} do
+      expect(@params).not_to include(:filters)
+    end
+    
+    it 'should allow override of default filters and options', :options => {:sort_by => 'awesomeness', :filters => {:l1 => true}} do
+      defaults = ChargePoint::API.send(:params_with_filters, {})
+      
+      expect(defaults[:f_l1]).to eq(false)
+      expect(@params[:f_l1]).to eq(true)
+      
+      expect(defaults[:sort_by]).to eq('distance')
+      expect(@params[:sort_by]).to eq('awesomeness')
+    end
+
+    it 'should add a fresh timestamp as _' do
+      expect(@params).to include(:_)
+      expect(@params[:_]).to be_within(2).of(Time.now.to_i)
+    end
   end
 
   context 'when not authenticated' do
@@ -36,6 +106,10 @@ RSpec.describe ChargePoint::API do
     end
 
     it {is_expected.not_to be_authenticated}
+
+    it 'should not allow authenticated methods to attempt to connect' do
+      expect {subject.get_charge_spots(1,2)}.to raise_error(RuntimeError, 'method requires authentication')
+    end
   end
 
   context 'while authenticating' do
@@ -67,5 +141,20 @@ RSpec.describe ChargePoint::API do
     subject {ChargePoint::API}
 
     it {is_expected.to be_authenticated}
+
+    context '.get_charge_spots' do
+      before :each do
+        double_agent
+        expect_get
+      end
+      
+      after :each do
+        undouble_agent
+      end
+      
+      it 'should construct and call the right URL' do
+        expect {subject.get_charge_spots(0,0,1)}.not_to raise_error
+      end
+    end
   end
 end
